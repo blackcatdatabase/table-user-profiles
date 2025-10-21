e2684f4ea85802e5c6da199b59d37c11a6bcf641
Tolben <vitek.pasek@gmail.com>
2025-10-22 00:01:07 +0200

    chore(schema): add/update generated schema + README (umbrella sync)

--- schema/001_table.sql
+++ schema/001_table.sql
@@ -1,4 +1,4 @@
--- Auto-generated from schema-map.psd1 on 2025-10-21T02:32:05
+-- Auto-generated from schema-map.psd1 @ 1e83bb6 (2025-10-21T10:18:36+02:00)
 -- table: user_profiles
 CREATE TABLE IF NOT EXISTS user_profiles (
   user_id BIGINT UNSIGNED PRIMARY KEY,
